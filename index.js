4ade8ea736fa6a191105393dd79b832955aafed0
Justin Mancherje <mancherje.justin@gmail.com>
2019-03-27 16:53:53 -0700

    Revert "testing for error codes (revert this)"
    This reverts commit bcf6da35e7019d179bb2022e92dc9c8d4593281c.

--- index.js
+++ index.js
@@ -49,8 +49,7 @@ function handleImage(image, callback, textResponseNumbers = {}) {
         url: `https://app.brandcast.io/api/_/images/${websiteId}/upload/`,
         data: form,
         headers: {
-          // "Content-Type": `multipart/form-data; boundary=${form._boundary}`,
-          "Content-Type": 'multipart/form-data',
+          "Content-Type": `multipart/form-data; boundary=${form._boundary}`,
           token
         }
       })
